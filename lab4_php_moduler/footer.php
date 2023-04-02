24e2ee68cb222f5226db42110326e61823c98258
asyhaul <116356016+asyhaul@users.noreply.github.com>
2023-04-02 16:08:03 +0700

    Add files via upload

---
+++ lab4_php_moduler/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+    <div class="container">
+       <footer>
+        <p>&copy; 2023, Informatika, Universitas Pelita Bangsa</p>
+       </footer>
+    </div>
+</body>
+</html>
